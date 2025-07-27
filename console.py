f23f0f6cd13051ada4350fb2127e3a8c89a3fe0d
David Idowu <idowudavidodun@gmail.com>
2025-07-27 15:02:49 +0100

    console

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" The entry point of the command interpreter """
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Command interpreter """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ Quit command to exit the program \n"""
+        return True
+
+    def do_EOF(self, arg):
+        """ Exit (Ctrl+D) to exit the program """
+        print("")
+        return True
+    
+    def emptyline(self, arg):
+        """ Do nothing on empty input line """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
