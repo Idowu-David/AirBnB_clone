a476e6b873b169a7840976217ee4028f8a6f64fa
David Idowu <idowudavidodun@gmail.com>
2025-07-28 19:11:40 +0100

    show and create

--- console.py
+++ console.py
@@ -3,7 +3,12 @@
 
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
+classes = {
+    "BaseModel": BaseModel
+}
 
 class HBNBCommand(cmd.Cmd):
     """ Command interpreter """
@@ -22,5 +27,40 @@ class HBNBCommand(cmd.Cmd):
         """ Do nothing on empty input line """
         pass
 
+    def do_create(self, arg):
+        """ Creates a new instance of BaseModel """
+        if not arg:
+            print(f"** class name missing **")
+        elif arg not in classes:
+            print(f"** class doesn't exist **")
+        else:
+            new = classes[arg]()
+            new.save()
+            print(new.id)
+
+    def do_show(self, arg):
+        """ Prints the string representaion of an instance based on the class name """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in classes:
+            print(" ** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        idd = args[1]
+        dicts = storage.all()
+        ids = [i.split(".")[1] for i in dicts.keys()]
+        if idd not in ids:
+            print("** no instance found **")
+            return
+        clss = class_name + "." + idd
+        print(f"{dicts[clss]}")
+        
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
